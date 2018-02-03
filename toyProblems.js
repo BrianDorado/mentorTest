c647b4d697e69bda8d577fe15c04e0b9c6ce23de
Brian Larson <manateeman24@hotmail.com>
2018-02-02 17:39:09 -0700

    finished written section

--- toyProblems.js
+++ toyProblems.js
@@ -1,36 +1,51 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: (str) => {
+    return str.split('').reverse().join('')
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups: function removeDups(arr) {
+    let newArr = arr.filter(function(val, ind){
+      return arr.indexOf(val) == ind
+    })
+      return newArr;
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt: () => {
+  
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter:(str) => {
+    return str.match(/[aeiou]/gi).length
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime:(n) => {
+    for (let i = 2; i < n; i ++) {
+
+    }
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
 
   //what is the value of foo?
-  //var foo = 10 + '20';
+  // var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020'
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
